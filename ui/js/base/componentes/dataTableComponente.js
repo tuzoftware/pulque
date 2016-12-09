a41a88e2da54937b36b53d04b3c78f6128261a90
tuzoftware <ultrafenixjf@gmail.com>
2016-12-09 15:38:25 -0600

    Soporte para enumerar los registros y refactor

--- ui/js/base/componentes/dataTableComponente.js
+++ ui/js/base/componentes/dataTableComponente.js
@@ -1,115 +1,94 @@
-var dataTableComponente = (function(selector,url,columnas,selectorFormulario) {
-    if(selectorFormulario === undefined){
-    	selectorFormulario="";
-	}
+var dataTableComponente = (function (selector, url, columnas, selectorFormulario, numerodeResultados) {
+    if (selectorFormulario === undefined) {
+        selectorFormulario = "";
+    }
     $.fn.dataTable.ext.errMode = 'throw';
-	//.on( 'error.dt', function ( e, settings, techNote, message ) {
-    //         console.log( 'Ocurrio un error: ', message );
-    //         })
-var tieneError=false;
-	var dt=$(selector).DataTable({"bFilter": false,"bLengthChange": false,"ordering": false,	"iDisplayLength": 10,	
-		     "language": {
-              "zeroRecords": "No se encontraron resultados",
-             "info": 'Mostrando resultados del <span class="label label-default">_PAGE_</span> al <span class="label      label-default">_PAGES_</span> de un total de <span class="label label-danger">_TOTAL_</span>',
-             "infoEmpty": "No se encontraron resultados",
-			  "paginate": {
-               "first":      "Primera",
-               "last":       "Ultima",
-               "next":       "Siguiente",
-               "previous":   "Anterior"
-              }
-            //"infoFiltered": "(filtered from _MAX_ total records)"
-            },
-			 "serverSide": true,			//Habilita el paginado por medio del servidor de aplicaciones
-                "fnDrawCallback": function (oSettings) {	//Metodo que se ejecuta al finalizar la peticion
-					$("#loading").hide();
-                    $(this).fadeIn();
-                    $('#buscar').empty().html('<i class="i-buscar"></i> Buscar').removeAttr('disabled'); 
-                },
-				 "preDrawCallback": function( settings ) {
-					  
-					 if(settings.json!==undefined){
-						 console.log(settings.json.tipoRespuesta);
-						  if(settings.json.tipoRespuesta!="DATOS" && tieneError){
-							  
-						    return false;	
-						  }
-					 }
-                },
-				"fnServerData": function ( sSource, aoData, fnCallback, oSettings ) {
-//aoData=aoData.concat( $(selectorFormulario).serializeArray() );
-  // aoData.push( { "name": "more_data", "value": "my_value" } );
-           var model = $(selectorFormulario).serializeArray();
-		 
-$.map(model, function (objeto, i) {
-                        //console.log(val);
-						//console.log(objeto["value"]);
-						  oSettings.oAjaxData[objeto["name"]]=objeto["value"];
-						//return model.push({ "name": "nombrePropiedadoControl[" + i + "]", "value": val });
-                    });
-		     //aoData.push( $(selectorFormulario).serializeObject() );
+    if (numerodeResultados === undefined) {
+        numerodeResultados = 10;
+    }
+    var tieneError = false;
+    var dt = $(selector).DataTable({
+        "bFilter": false, "bLengthChange": false, "ordering": false, "iDisplayLength": numerodeResultados,
+        "language": {
+            "zeroRecords": "No se encontraron resultados",
+            "info": 'Mostrando resultados del <span class="label label-default">_PAGE_</span> al <span class="label      label-default">_PAGES_</span> de un total de <span class="label label-danger">_TOTAL_</span>',
+            "infoEmpty": "No se encontraron resultados",
+            "paginate": {
+                "first": "Primera",
+                "last": "Ultima",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "serverSide": true,
+        //Habilita el paginado por medio del servidor de aplicaciones
+        "fnDrawCallback": function () {
+            //Metodo que se ejecuta al finalizar la peticion
+            $("#loading").hide();
+            $(this).fadeIn();
+            $('#buscar').empty().html('<i class="i-buscar"></i> Buscar').removeAttr('disabled');
+        },
+        "preDrawCallback": function (settings) {
+            if (settings.json !== undefined && settings.json.tipoRespuesta != "DATOS" && tieneError) {
+                    return false;
+            }
+        },
+        "fnServerData": function (sSource, aoData, fnCallback, oSettings) {
+            var model = $(selectorFormulario).serializeArray();
+            $.map(model, function (objeto) {
+                oSettings.oAjaxData[objeto["name"]] = objeto["value"];
+            });
+            $.ajax({
+                "dataType": 'json',
+                "type": "POST",
+                "url": url,
+                "data": oSettings.oAjaxData,
+                // "data" :$.makeArray( aoData ),
+                "success": function (json) {
+                    /* Do whatever additional processing you want on the callback,
+                     then tell DataTables */
+                    if (json.tipoRespuesta === "DATOS") {
+                        fnCallback(json);
+                    }
 
-  
-  //console.log(aoData);
-  //aoData.push( { "filtros[nombre_busqueda]": "more_data", "value": "my_value" } );				             
+                }
+            });
 
-							 $.ajax( {
-         "dataType" : 'json',
-         "type" : "POST",
-         "url" : url,
-		 "data": oSettings.oAjaxData,
-     // "data" :$.makeArray( aoData ),
-         "success" : function(json) {
-           /* Do whatever additional processing you want on the callback, 
-             then tell DataTables */
-			 if(json.tipoRespuesta==="DATOS"){
-				 fnCallback(json);
-			 }
-           
-       }
-	   });
-                           
-				},
-			
-                "columns":columnas
+        },
+
+        "columns": columnas
     });
-	dt.on( 'xhr', function ( e, settings, json, xhr ) {
-  //console.log(e);
- if ( json === null ) {
-     // custom error reporting if needed
-    return true;
-  }else if(json.tipoRespuesta!="DATOS"){
-	//dt.preDrawCallback(false);
-	//e.stopPropagation();
-	//console.log("**************************");
-return true;
-  }
-} );
+    dt.on('xhr', function (e, settings, json) {
+        if (json === null || json.tipoRespuesta != "DATOS") {
+            // custom error reporting if needed
+            return true;
+        }
+    });
 
 
-    
-	function buscar(){
-		$(".errores").hide();
-		$(".errores i").html("");
-		dt.draw();
-	}
-
-	function obtenerSeleccionado(elemento) {
-		var renglon=$(elemento).closest("tr");
-		var row = dt.row(renglon);
-		return row.data();
-	}
-
-    function recargar(){
+    function buscar() {
+        $(".errores").hide();
+        $(".errores i").html("");
         dt.draw();
     }
-	
-	//Inicio Return
-	return {
-		buscar:buscar,
-		obtenerSeleccionado : obtenerSeleccionado,
-		recargar: recargar
-	};
 
-	//Fin Return
+    function obtenerSeleccionado(elemento) {
+        var renglon = $(elemento).closest("tr");
+        var row = dt.row(renglon);
+        return row.data();
+    }
+
+    function recargar() {
+        dt.draw();
+    }
+
+
+    //Inicio Return
+    return {
+        buscar: buscar,
+        obtenerSeleccionado: obtenerSeleccionado,
+        recargar: recargar
+    };
+
+    //Fin Return
 });
